821950d0887c91270a7f098156f167ee4d0b7e77
m.romanovsky@z-labs.ru <m.romanovsky@z-labs.ru>
2022-11-01 14:21:52 +0900

    progress

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pygame
 
-FRAME_COLOR = (0, 255, 204)
-WHITE = (255, 255, 255)
-BLUE = (204, 255, 255)
+FRAME_COLOR = (18, 27, 87)
+BLUE = (195, 198, 219)
+RED = (252, 130, 116)
 SIZE_BLOCK = 20
 COUNT_BLOCKS = 20
-MARGIN = 1
+MARGIN = 2
 
-size = [400, 600]
+size = [500, 600]
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Супер Змейка")
 
@@ -18,11 +18,13 @@ while True:
 
     screen.fill(FRAME_COLOR)
     for row in range(COUNT_BLOCKS):
-        for column in range(0, COUNT_BLOCKS):
-            if column % 2 == 0:
-                color = BLUE
+        for column in range(COUNT_BLOCKS):
+            if (column + row) % 2 == 0:
+                color = RED
             else:
-                color = WHITE
-            pygame.draw.rect(screen, color, [10 + column * SIZE_BLOCK + MARGIN * column, 20, SIZE_BLOCK, SIZE_BLOCK])
+                color = BLUE
+            pygame.draw.rect(screen, color,
+                             [10 + column * SIZE_BLOCK + MARGIN * column, 20 + row * SIZE_BLOCK + MARGIN * row,
+                              SIZE_BLOCK, SIZE_BLOCK])
 
     pygame.display.flip()
